f4f01838ca19b3e905c98c1d96393cc3a0719058
Yu <yux.uth@gmail.com>
2025-02-15 18:32:41 -0800

    Update 1539.kth-missing-positive-number.go

--- TwoPointer/1539.kth-missing-positive-number.go
+++ TwoPointer/1539.kth-missing-positive-number.go
@@ -34,3 +34,43 @@ func findKthPositive(arr []int, k int) int {
 // }
 // @leet end
 
+package leetcode
+
+// @leet start
+func findKthPositive(arr []int, k int) int {
+	l, r, idx := 0, len(arr)-1, -1
+	for l <= r {
+    mid := (l+r) >> 1
+    if arr[mid] - (mid +1) < k {
+      idx = mid // NOTE: binary serarch
+      l = mid +1
+    } else {
+      r = mid -1
+    }
+	}
+	return idx + 1 + k
+}
+
+
+func findKthPositive2(arr []int, k int) int {
+	var missed int
+	if arr[0] != 1 {
+		missed = arr[0] - 1
+	}
+	if k <= missed {
+		return k
+	}
+	for i := 1; i < len(arr); i++ {
+		if arr[i] != arr[i-1]+1 {
+			miss := arr[i] - arr[i-1] - 1
+			if missed+miss >= k {
+				return arr[i-1] + k - missed
+			} else {
+				missed += miss
+			}
+		}
+	}
+	return arr[len(arr)-1] + k - missed
+}
+
+
