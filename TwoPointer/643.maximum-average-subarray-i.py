24a880bee0301405ffffe91f6f647019b9cc7435
Yu <yux.uth@gmail.com>
2025-02-17 14:40:54 -0800

    Update 643.maximum-average-subarray-i.py

--- TwoPointer/643.maximum-average-subarray-i.py
+++ TwoPointer/643.maximum-average-subarray-i.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from math import inf
 
 # @leet start
 class Solution:
@@ -15,3 +15,28 @@ class Solution:
 
 # @leet end
 
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        total = 0
+        for i, v in enumerate(nums):  # NOTE: miss last item
+            total += v
+            if i == k - 1:
+                best = total  # NOTE: wield condition
+            if i >= k:
+                total -= nums[i - k]
+                best = max(total, best)
+        return float(best) / float(k)
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        total = 0
+        best = None
+        for i, v in enumerate(nums):
+            if i < k:  # NOTE: incorrect
+                total += v
+            else:
+                if best is None or total > best:
+                    best = total
+                total -= nums[i - k]
+                total += v
+        return float(best) / float(k)
