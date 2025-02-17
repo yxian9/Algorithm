6027524bfffe6f5c37c5be55ed352e4e777c3937
Yu <yux.uth@gmail.com>
2025-02-17 13:16:06 -0800

    Update 958.check-completeness-of-a-binary-tree.go

--- BFS/958.check-completeness-of-a-binary-tree.go
+++ BFS/958.check-completeness-of-a-binary-tree.go
@@ -66,3 +66,52 @@ func isCompleteTree(root *TreeNode) bool {
 	}
 	return true
 }
+
+func isCompleteTree(root *TreeNode) bool {
+	count, id := -1, 0 // NOTE: 1, 0, count need -1, id start from 0 is fixed
+	var dfs func(*TreeNode, int)
+	dfs = func(node *TreeNode, n int) {
+		if node == nil {
+			return
+		}
+		id = max(id, n)
+		count++
+		dfs(node.Left, 2*n+1)
+		dfs(node.Right, 2*n+2)
+	}
+	dfs(root, 0)
+	return count == id
+}
+
+// @leet end
+
+func isCompleteTree2(root *TreeNode) bool { // NOTE: logic flaw. dfs is hard to aware the status on other branch
+	var (
+		first = true
+		dfs   func(*TreeNode) bool
+	)
+	dfs = func(node *TreeNode) bool {
+		if node == nil {
+			return true
+		}
+		if node.Left == nil && node.Right == nil {
+			return true
+		}
+		if node.Left != nil && node.Right != nil {
+			return dfs(node.Left) && dfs(node.Right)
+		}
+		if node.Left == nil {
+			return false
+		}
+		if first {
+			first = false
+			return dfs(node.Left)
+		} else {
+			return false
+		}
+	}
+
+	return dfs(root)
+}
+
+
