eace71efdc55e499226cbd02288dbe4476ed3292
Yu <yux.uth@gmail.com>
2025-02-27 08:35:38 -0800

    Update 62.unique-paths.py

--- DynamicProgramming/62.unique-paths.py
+++ DynamicProgramming/62.unique-paths.py
@@ -14,3 +14,15 @@ class Solution:
 
 # @leet end
 
+class Solution: # NOTE: use dfs to init 
+    def uniquePaths(self, m: int, n: int) -> int:
+        @cache
+        def dp(i, j):
+            if i == 0:
+                return 1
+            if j == 0:
+                return 1
+            return dp(i - 1, j) + dp(i, j - 1)
+
+        return dp(m - 1, n - 1)
+
