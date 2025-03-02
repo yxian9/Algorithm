61477c9e6671275c799ba98375e5b6a5433aaef0
Yu <yux.uth@gmail.com>
2025-03-02 12:04:55 -0800

    Update 739.daily-temperatures.py

--- Stack/739.daily-temperatures.py
+++ Stack/739.daily-temperatures.py
@@ -1,6 +1,22 @@
 from typing import List
 
+def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        ret = [0] * n
+        st = []
+        for i in range(n - 1, -1, -1):
+            # NOTE: dirction is opposite to (next) greater/smaller
+            # when do compare, put the need update item to left side
+            while st and temperatures[st[-1]] <= temperatures[i]:
+                st.pop()
 
+            if st:
+                ret[i] = st[-1] - i
+            else:
+                ret[i] = 0
+            st.append(i)
+        return ret
+    
 # @leet start
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
